bab21102ca43c45532de1d282758c75ac17d63e3
tatiang <tatiangreenleaf@gmail.com>
2026-01-24 21:00:59 -0800

    Update version_test_Gemini.js v1.96

--- js/version_test_Gemini.js
+++ js/version_test_Gemini.js
@@ -1,6 +1,6 @@
 /* Star Academy Version Control */
 
-const CURRENT_VERSION = "v1.95";
+const CURRENT_VERSION = "v1.96";
 const BUILD_DATE = new Date().toLocaleDateString();
 
 console.log(`Version Controller: Loaded ${CURRENT_VERSION}`);
