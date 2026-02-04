55f5eede6315c79fd6f8a6779aa1fc50bcbc051b
tatiang <tatiangreenleaf@gmail.com>
2026-02-04 12:41:55 -0800

    v2.50 - kiosk grid iPad layout

--- js/variables_test_Gemini.js
+++ js/variables_test_Gemini.js
@@ -4,7 +4,7 @@
 
 window.app = {
     version: "v2.50", // Updated Version
-    lastModified: "2026-02-04T11:52:02-08:00",
+    lastModified: "2026-02-04T12:41:29-08:00",
     storageKey: "star_pos_v2_data",
     taxRate: 0.0925,
     session: {
